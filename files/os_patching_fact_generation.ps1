c536bede6a6801e0e555d6eecc27c488facf4320
Tony Green <tgreen@bandcamp.tv>
2018-10-04 21:53:46 +1000

    try this

--- files/os_patching_fact_generation.ps1
+++ files/os_patching_fact_generation.ps1
@@ -7,7 +7,7 @@ Get-WUServiceManager | Where-Object {$_.IsManaged -eq 'true'} | ForEach-Object {
 	{
 		'3da21691-e39d-4da6-8a4b-b43877bcb1b7' { Get-WUList | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
 		'9482f4b4-e343-43b6-b170-9a65bc822c77' { Get-WUList -WindowsUpdate | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
-		'7971f918-a847-4430-9279-4a52d1efe18d' { Get-WUList -MicrosoftUpdate | Format-List -Property Title > C:\ProgramData\os_patching\package_updates
+		'7971f918-a847-4430-9279-4a52d1efe18d' { Get-WUList -MicrosoftUpdate | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
 		default { Get-WUList | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
 	}
 }
