004fb2ff7fc34fb7e0fb49bce4c916d057e692cd
Tony Green <tgreen@bandcamp.tv>
2018-10-04 21:14:22 +1000

    try this

--- files/os_patching_fact_generation.ps1
+++ files/os_patching_fact_generation.ps1
@@ -1,1 +1,11 @@
-$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8';Import-Module PSWindowsUpdate; Get-WUList -WindowsUpdate | Format-List -Property Title > C:\ProgramData\os_patching\package_updates
+
+$mode = Get-WUServiceManager | Where-Object {$_.IsManaged -eq 'true'} | foreach {$_.ServiceID}
+
+switch ( $mode )
+{
+	'3da21691-e39d-4da6-8a4b-b43877bcb1b7' { $type = '' }
+	'9482f4b4-e343-43b6-b170-9a65bc822c77' { $type = -WindowsUpdate }
+	'7971f918-a847-4430-9279-4a52d1efe18d' { $type = -MicrosoftUpdate }
+}
+
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8';Import-Module PSWindowsUpdate; Get-WUList $type | Format-List -Property Title > C:\ProgramData\os_patching\package_updates
