fdcc27017fee59e559a02831547d5d73e1f54491
Tony Green <tgreen@bandcamp.tv>
2018-10-04 22:24:48 +1000

    try this

--- files/os_patching_fact_generation.ps1
+++ files/os_patching_fact_generation.ps1
@@ -1,5 +1,6 @@
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 Import-Module PSWindowsUpdate
+Clear-Content 'C:\ProgramData\os_patching\package_updates' -Force
 
 #Get-WUServiceManager | Where-Object {$_.IsManaged -match 'true'} | ForEach-Object {
 Get-WUServiceManager | Where-Object {$_.IsManaged -match 'false'} | ForEach-Object {
@@ -7,10 +8,10 @@ Get-WUServiceManager | Where-Object {$_.IsManaged -match 'false'} | ForEach-Obje
   Write-Host $_.ServiceID
 	switch ( $_.ServiceID )
 	{
-		'3da21691-e39d-4da6-8a4b-b43877bcb1b7' { Get-WUList | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
-		'9482f4b4-e343-43b6-b170-9a65bc822c77' { Get-WUList -WindowsUpdate | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
-		'7971f918-a847-4430-9279-4a52d1efe18d' { Get-WUList -MicrosoftUpdate | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
-		default { Get-WUList | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
+		'3da21691-e39d-4da6-8a4b-b43877bcb1b7' { Get-WUList | Format-List -Property Title | Out-File -Append C:\ProgramData\os_patching\package_updates }
+		'9482f4b4-e343-43b6-b170-9a65bc822c77' { Get-WUList -WindowsUpdate | Format-List -Property Title | Out-File -Append C:\ProgramData\os_patching\package_updates }
+		'7971f918-a847-4430-9279-4a52d1efe18d' { Get-WUList -MicrosoftUpdate | Format-List -Property Title | Out-File -Append C:\ProgramData\os_patching\package_updates }
+		default { Get-WUList | Format-List -Property Title | Out-File -Append C:\ProgramData\os_patching\package_updates }
 	}
 }
 
