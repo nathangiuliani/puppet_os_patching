8591cd40f2f9b7777427b823333af1c518f4d35c
Tony Green <tgreen@bandcamp.tv>
2018-10-04 21:21:49 +1000

    try this

--- files/os_patching_fact_generation.ps1
+++ files/os_patching_fact_generation.ps1
@@ -6,6 +6,7 @@ switch ( $mode )
 	'3da21691-e39d-4da6-8a4b-b43877bcb1b7' { $type = '-WindowsUpdate' }
 	'9482f4b4-e343-43b6-b170-9a65bc822c77' { $type = '-WindowsUpdate' }
 	'7971f918-a847-4430-9279-4a52d1efe18d' { $type = '-MicrosoftUpdate' }
+	default { $type = '-MicrosoftUpdate' }
 }
 
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8';Import-Module PSWindowsUpdate; Get-WUList $type | Format-List -Property Title > C:\ProgramData\os_patching\package_updates
