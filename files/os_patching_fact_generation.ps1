225bd931eb94eb87131e82ca798712e403661a66
Tony Green <tgreen@bandcamp.tv>
2018-10-04 22:15:50 +1000

    try this

--- files/os_patching_fact_generation.ps1
+++ files/os_patching_fact_generation.ps1
@@ -4,8 +4,8 @@ Import-Module PSWindowsUpdate
 #Get-WUServiceManager | Where-Object {$_.IsManaged -match 'true'} | ForEach-Object {
 Get-WUServiceManager | Where-Object {$_.IsManaged -match 'false'} | ForEach-Object {
   Write-Host "Inside the loop"
-  Write-Host $_
-	switch ( $_ )
+  Write-Host $_.ServiceID
+	switch ( $_.ServiceID )
 	{
 		'3da21691-e39d-4da6-8a4b-b43877bcb1b7' { Get-WUList | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
 		'9482f4b4-e343-43b6-b170-9a65bc822c77' { Get-WUList -WindowsUpdate | Format-List -Property Title > C:\ProgramData\os_patching\package_updates }
