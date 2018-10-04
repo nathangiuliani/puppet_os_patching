21da2e3aa2f7ac49dacd4de43e837f287faec168
Tony Green <tgreen@bandcamp.tv>
2018-10-04 21:38:03 +1000

    try this

--- files/os_patching_fact_generation.ps1
+++ files/os_patching_fact_generation.ps1
@@ -15,4 +15,4 @@ switch ( $mode )
 
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 Import-Module PSWindowsUpdate
-Get-WUList $type | Format-List -Property Title > C:\ProgramData\os_patching\package_updates
+Get-WUList -WindowsUpdate | Format-List -Property Title > C:\ProgramData\os_patching\package_updates
