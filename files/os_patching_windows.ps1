166b685eada364c2b3cbfaa8c04721451bb20cf4
Nathan Giuliani <nathan.giuliani@nec.com.au>
2019-01-27 22:35:43 +1030

    functionised, initial update install code

--- files/os_patching_windows.ps1
+++ files/os_patching_windows.ps1
@@ -1,258 +1,331 @@
 #Requires -Version 3.0
 
-[CmdletBinding(DefaultParameterSetName = 'DefaultUpdateService')]
+[CmdletBinding()]
 param(
-    [Parameter(ParameterSetName = 'WindowsUpdate')]
-    [Parameter(ParameterSetName = 'MicrosoftUpdate')]
-    [Parameter(ParameterSetName = 'DefaultUpdateService')]
-    [Switch]
-    $RefreshFacts,
-
-    [Parameter(ParameterSetName = 'WindowsUpdate')]
-    [Parameter(ParameterSetName = 'MicrosoftUpdate')]
-    [Parameter(ParameterSetName = 'DefaultUpdateService')]
-    [Switch]
-    $SecurityOnly,
-
-    [Parameter(ParameterSetName = 'WindowsUpdate')]
-    [Switch]
-    $UseWindowsUpdate,
-    
-    [Parameter(ParameterSetName = 'MicrosoftUpdate')]
-    [Switch]
-    $UseMicrosoftUpdate
+    [Switch]$RefreshFacts,
+    [Switch]$SecurityOnly,
+    [String]$updateCriteria = "IsInstalled=0 and IsHidden=0"
 )
 
-#$VerbosePreference = "continue"
+$VerbosePreference = "continue"
 
-#strict mode
+# strict mode
 Set-StrictMode -Version 2
+
+# clear any errors
+$error.Clear()
+
+# Set error action preference to stop. Trap ensures all errors caught
+$ErrorActionPreference = "stop"
+
+# trap
+trap {
+    # verbose output for console
+    Write-Verbose "Unhandled exception caught:"
+    Write-Verbose $_.exception.ToString()                                          # Error message
+    Write-Verbose $_.invocationinfo.positionmessage.ToString()                     # Line the error was generated on
+
+    # JSON output for bolt etc.
+    $trapDetails = "Failed due to trap - {0} {1}" -f $_.exception.ToString() , $_.invocationinfo.positionmessage.ToString() 
+
+    [PSCustomObject]@{
+        Status = "Failure"
+        Details = $trapDetails
+    } | ConvertTo-Json
+}
 
 #
 # functions
 #
 
-Function Get-WUServiceManager {
-    New-Object -ComObject 'Microsoft.Update.ServiceManager'
-}
-
 Function Get-WUSession {
+    # returns a microsoft update session object
+    Write-Debug "Get-WUSession: Creating update session object"
     New-Object -ComObject 'Microsoft.Update.Session' 
 }
 
 Function Get-WUUpdateCollection {
+    #returns a microsoft update update collection object
     New-Object -ComObject Microsoft.Update.UpdateColl
 }
 
-Function Get-WUServiceManager {
-    <#
-	.SYNOPSIS
-	    Show Service Manager configuration.
-
-	.DESCRIPTION
-	    Use Get-WUServiceManager to get available configuration of update services.
-                              		
-	.EXAMPLE
-		Show currently available Windows Update Services on machine.
-	
-		PS C:\> Get-WUServiceManager
-
-		ServiceID                            IsManaged IsDefault Name
-		---------                            --------- --------- ----
-		9482f4b4-e343-43b6-b170-9a65bc822c77 False     False     Windows Update
-		7971f918-a847-4430-9279-4a52d1efe18d False     False     Microsoft Update
-		3da21691-e39d-4da6-8a4b-b43877bcb1b7 True      True      Windows Server Update Service
-		13df3d8f-78d7-4eb8-bb9c-2a101870d350 False     False     Offline Sync Service2
-		a8f3b5e6-fb1f-4814-a047-2257d39c2460 False     False     Offline Sync Service
-
-	.NOTES
-		Author: Michal Gajda
-		Blog  : http://commandlinegeeks.com/
-		
-	.LINK
-		http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc
-
-	.LINK
-        Add-WUOfflineSync
-        Remove-WUOfflineSync
-	#>
-    [OutputType('PSWindowsUpdate.WUServiceManager')]
-    [CmdletBinding(
-        SupportsShouldProcess = $True,
-        ConfirmImpact = "Low"
-    )]
-    Param()
-	
-    Begin {
-        $User = [Security.Principal.WindowsIdentity]::GetCurrent()
-        $Role = (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-
-        if (!$Role) {
-            Write-Warning "To perform some operations you must run an elevated Windows PowerShell console."	
-        } #End If !$Role	
-    }
-	
-    Process {
-        If ($pscmdlet.ShouldProcess($Env:COMPUTERNAME, "Get Windows Update ServiceManager")) {
-            $objServiceManager = New-Object -ComObject "Microsoft.Update.ServiceManager"
-
-            $ServiceManagerCollection = @()
-            Foreach ($objService in $objServiceManager.Services) {
-                $objService.PSTypeNames.Clear()
-                $objService.PSTypeNames.Add('PSWindowsUpdate.WUServiceManager')
-						
-                $ServiceManagerCollection += $objService
-            } #End Foreach $objService in $objServiceManager.Services
-			
-            Return $ServiceManagerCollection
-        } #End If $pscmdlet.ShouldProcess($Env:COMPUTERNAME,"Get Windows Update ServiceManager")		
-
-    } #End Process
-	
-    End {}
-} #In The End :)
-
-Function Get-Pendingreboot {
+Function Get-PendingReboot {
     #Copied from http://ilovepowershell.com/2015/09/10/how-to-check-if-a-server-needs-a-reboot/
     #Adapted from https://gist.github.com/altrive/5329377
     #Based on <http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542>
 
-    if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
-    if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
-    if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
+    $rebootPending = $false
+
+    if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { $rebootPending = $true }
+    if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { $rebootPending = $true }
+    if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { $rebootPending = $true }
     try { 
         $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
         $status = $util.DetermineIfRebootPending()
         if (($null -ne $status) -and $status.RebootPending) {
-            return $true
+            $rebootPending = $true
         }
     }
     catch {}
  
-    return $false
+    if ($rebootPending) { Write-Verbose "A reboot is required" }
+
+    # return result
+    $rebootPending
 }
+
+Function Invoke-RefreshPuppetFacts{
+    # refreshes puppet facts used by os_patching module
+    # inputs - $UpdateSession - microsoft update session object
+    # outpts - none, saves puppet fact files only
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$UpdateSession
+    )
+    # refresh puppet facts
+
+    Write-Verbose "Refresing puppet facts"
+
+    $allUpdates = Get-UpdateSearch($UpdateSession)
+    $securityUpdates = Get-SecurityUpdates($allUpdates)
+
+    #paths to facts
+    $dataDir = 'C:\ProgramData\os_patching'
+    $updateFile = Join-Path -Path $dataDir -ChildPath 'package_updates'
+    $secUpdateFile = Join-Path -Path $dataDir -ChildPath 'security_package_updates'
+    $rebootReqdFile = Join-Path -Path $dataDir -ChildPath  'reboot_required'
+
+    # output list of required updates
+    $allUpdates | Select-Object -ExpandProperty Title | Out-File $updateFile -Encoding ascii
+
+    # filter to security updates and output
+    $securityUpdates | Select-Object -ExpandProperty Title | Out-File $secUpdateFile -Encoding ascii
+
+    # get pending reboot details
+    Get-PendingReboot | Out-File $rebootReqdFile -Encoding ascii
+
+    # upload facts
+    Write-Verbose "Uploading puppet facts"
+    $puppetCmd = Join-Path $env:ProgramFiles -ChildPath "Puppet Labs\Puppet\bin\puppet.bat"
+    & $puppetCmd facts upload
+}
+
+Function Get-UpdateSearch{
+    # performs an update search
+    # inputs: update session
+    # outputs: updates from search result
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]$UpdateSession
+    )
+
+    Write-Verbose "Update search criteria is: $updateCriteria"
+
+    # create update searcher
+    $updateSearcher = $UpdateSession.CreateUpdateSearcher()
+
+    Write-Verbose "Performing update search"
+
+    # perform search and select Update property
+    $updates = $updateSearcher.Search($updateCriteria).Updates
+
+    $updateCount = $updates.count
+
+    Write-Verbose "Detected $updateCount updates are required in total (including security)"
+
+    # return updates
+    $updates
+}
+
+Function Get-SecurityUpdates{
+    # filters update list to security only
+    # inputs - update list from an update search
+    # outputs - filtered list
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$Updates
+    )
+    # filter to security updates
+    $secUpdates = $Updates | Select-Object Title, @{N = "categories"; E = {$_.Categories | Select-Object -expandproperty Name}} | Where-Object {$_.categories -contains "Security Updates"}
+
+    # count them
+    $secUpdateCount = $secUpdates.count
+
+    Write-Verbose "Detected $secUpdateCount security updates are required"
+
+    # return security updates
+    $secUpdates
+}
+
+Function Invoke-UpdateRun{
+    # perform an update run
+    # inputs - update session
+    # outputs - update run results
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$UpdateSession
+    )
+
+    # search for (all) updates
+    $allUpdates = Get-UpdateSearch($UpdateSession)
+
+    # filter to security updates if switch parameter is set
+    if ($SecurityOnly) {
+        $updatesToInstall = Get-SecurityUpdates -Updates $allUpdates | Select-Object -first 3
+    }
+    else {
+        $updatesToInstall = $allUpdates | Select-Object -first 3
+    }
+
+    # get update count
+    $updateCount = @($updatesToInstall).count # ensure it's an array so count property exists
+
+    if ($updateCount -gt 0) { 
+        # we need to install updates
+
+        # download updates if needed. No output from this function
+        Invoke-DownloadUpdates -UpdateSession $UpdateSession -UpdatesToDownload $updatesToInstall
+
+        # Install Updates. Pass (return) output to the pipeline
+        Invoke-InstallUpdates -UpdateSession $UpdateSession -UpdatesToInstall $updatesToInstall
+    } else {
+        Write-Verbose "No updates required"
+
+        # build final result for output
+        [PSCustomObject]@{
+            Status = "No updates required"
+        }
+    }
+}
+
+Function Invoke-DownloadUpdates{
+    # download updates if required
+    # inputs  - UpdateSession     - update session
+    #         - UpdatesToDownload - update collection (of updates to download)
+    # outputs - none
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$UpdateSession,
+        [Parameter(Mandatory = $true)]$UpdatesToDownload
+    )
+
+    # download updates if necessary, i.e. only those where IsDownloaded is false
+    $updatesNotDownloaded = $UpdatesToDownload | Where-Object {$_.IsDownloaded -eq $false}
+
+    if ($updatesNotDownloaded) {
+        # Create update collection...
+        $updateDownloadCollection = Get-WUUpdateCollection
+
+        # ...Add updates to it
+        foreach ($update in $updatesNotDownloaded) {
+            [void]$updateDownloadCollection.Add($update) # void stops output to console
+        }
+
+        Write-Verbose "Downloading $($updateDownloadCollection.Count) updates"
+
+        # Create update downloader
+        $updateDownloader = $updateSession.CreateUpdateDownloader()
+
+        # Set updates to download
+        $updateDownloader.Updates = $updateDownloadCollection
+
+        # and download 'em!
+        [void]$updateDownloader.Download()
+        Write-Verbose "Download completed"
+    }
+    else {
+        Write-Verbose "All updates are already downloaded"
+    }
+}
+
+Function Invoke-InstallUpdates{
+    # install updates
+    # inputs  - UpdateSession    - update session
+    #         - UpdatesToInstall - update collection (of updates to install)
+    # outputs - pscustomobject with install results
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$UpdateSession,
+        [Parameter(Mandatory = $true)]$UpdatesToInstall
+    )
+
+    # get update count
+    $updateCount = @($updatesToInstall).count # ensure it's an array so count property exists
+
+    Write-Verbose "Installing $updateCount updates"
+
+    # create a counter var starting at 1
+    $counter = 1
+
+    # create blank array for result output
+    $updateInstallResults = @()
+
+    foreach ($update in $updatesToInstall) {
+
+        Write-Verbose "Installing update $($counter): $($update.Title)"
+
+        # create update collection...
+        $updateInstallCollection = Get-WUUpdateCollection
+
+        # ...Add the current update to it
+        [void]$updateInstallCollection.Add($update) # void stops output to console
+
+        # create update installer
+        $updateInstaller = $updateSession.CreateUpdateInstaller()
+
+        # Add update collection to the installer
+        $updateInstaller.Updates = $updateInstallCollection
+
+        # Install updates and capture result
+        $updateInstallResult = $updateInstaller.Install()
+
+        # Convert ResultCode to something readable
+        $updateStatus = switch ($updateInstallResult.ResultCode) {
+            0 { "NotStarted" }
+            1 { "InProgress" }
+            2 { "Succeeded" }
+            3 { "SucceededWithErrors" }
+            4 { "Failed" }
+            5 { "Aborted" }
+            default {"unknown"}
+        }
+
+        # build object with result for this update and add to array
+        $updateInstallResults += [pscustomobject]@{
+            Title          = $update.Title
+            Status         = $updateStatus
+            HResult        = $updateInstallResult.HResult
+            RebootRequired = $updateInstallResult.RebootRequired
+        }
+
+        # increment counter
+        $counter++
+    }
+
+    # build final result for output
+    [PSCustomObject]@{
+        Status = "Success"
+        InstallResults = $updateInstallResults
+        RebootRequired = Get-PendingReboot
+    } 
+}
+
 
 #
 # vars
 #
 
-# Update Criteria
-$updateCriteria = "IsInstalled=0 and IsHidden=0"
-
-#start time
-$startTime = [System.DateTime]::Now
-
-#paths to facts
-$dataDir = 'C:\ProgramData\os_patching'
-$updateFile = Join-Path -Path $dataDir -ChildPath 'package_updates'
-$secUpdateFile = Join-Path -Path $dataDir -ChildPath 'security_package_updates'
-$rebootReqdFile = Join-Path -Path $dataDir -ChildPath  'reboot_required'
-
-Write-Verbose "Update criteria is: $updateCriteria"
-
-Write-Verbose "Creating Objects"
+Write-Verbose "OS_Patching_Windows script started"
 
 #create update session
-$updateSession = Get-WUSession
+$wuSession = Get-WUSession
 
-#create update searcher
-$updateSearcher = $updateSession.CreateUpdateSearcher()
+if ($RefreshFacts) {
+    # refresh facts mode
+    Invoke-RefreshPuppetFacts -UpdateSession $wuSession
+} else {
+    # invoke update run    
+    Invoke-UpdateRun -UpdateSession $wuSession
+}           
 
-Write-Verbose "Selecting update service manager"
-
-# https://docs.microsoft.com/en-us/windows/desktop/api/wuapicommon/ne-wuapicommon-tagserverselection
-# https://docs.microsoft.com/en-us/previous-versions/windows/desktop/aa387280%28v%3dvs.85%29
-#
-# ssDefault        = 0,
-# ssManagedServer  = 1,
-# ssWindowsUpdate  = 2,
-# ssOthers         = 3
-
-if ($UseWindowsUpdate) {
-    Write-Verbose "Using Microsoft Update"
-    $updateSearcher.ServerSelection = 2 #ssWindowsUpdate
-}
-elseif ($UseMicrosoftUpdate) {
-    Write-Verbose "Using Windows Update"
-    $updateSearcher.ServerSelection = 3 #ssOthers
-    #
-    # MORE CODING TO DO HERE TO SET SERVICE ID :)
-    #
-}
-else {
-    $defaultUpdateService = Get-WUServiceManager | Where-Object {$_.IsDefaultAUService}
-    Write-Verbose "Using default update service - $($defaultUpdateService.name)"
-}
-
-Write-Verbose "Performing update search"
-$searchResults = $updateSearcher.Search($updateCriteria)
-
-# get updates
-$updates = $searchResults.updates
-
-$updateCount = $updates.count
-
-if ($updateCount -gt 0) {
-    Write-Verbose "Detected $updateCount updates."
-
-    if ($RefreshFacts) {
-        # output list of required updates
-        $updates | Select-Object -ExpandProperty Title | Out-File $updateFile -Encoding ascii
-
-        # filter to security updates and output
-        $updates | Select-Object Title, @{N = "categories"; E = {$_.Categories | Select-Object -expandproperty Name}} | `
-            Where-Object {$_.categories -contains "Security Updates"} | Select-Object -ExpandProperty Title | Out-File $secUpdateFile -Encoding ascii
-
-        Get-Pendingreboot | Out-File $rebootReqdFile -Encoding ascii
-
-        #upload facts
-        Write-Verbose "Uploading puppet facts"
-        $puppetCmd = Join-Path $env:ProgramFiles -ChildPath "Puppet Labs\Puppet\bin\puppet.bat"
-        & $puppetCmd facts upload
-    }
-    else {
-
-        # get updates to download
-        # only those where IsDownloaded is false
-        $updatesNotDownloaded = $updates | Where-Object {$_.IsDownloaded -eq $false}
-
-        if ($updatesNotDownloaded) {
-            # Create update collection...
-            $updateDownloadCollection = Get-WUUpdateCollection
-
-            # ...Add updates to it
-            foreach ($update in $updatesNotDownloaded) {
-                [void]$updateDownloadCollection.Add($update)
-            }
-
-            Write-Verbose "Downloading $($updateDownloadCollection.Count) updates."
-
-            # Create update downloader
-            $updateDownloader = $updateSession.CreateUpdateDownloader()
-
-            # Set updates to download
-            $updateDownloader.Updates = $updateDownloadCollection
-
-            # and download 'em!
-            [void]$updateDownloader.Download()
-            Write-Verbose "Download of $($updateDownloadCollection.Count) updates completed."
-        }
-        else {
-            Write-Verbose "All updates are already downloaded."
-        }
-
-        #end time
-        $endTime = [System.DateTime]::Now
-
-        #result
-        [pscustomobject]@{
-            StartTime = $startTime
-            EndTime   = $endTime
-        }
-
-    }
-}
-else {
-    Write-Verbose "No updates detected."
-}
-
+Write-Verbose "OS_Patching_Windows script finished"
