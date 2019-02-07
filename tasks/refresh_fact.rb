68f7e5f9c9317ea7aa6e6a56e11d9ef1143a8c88
Nathan Giuliani <nathan.giuliani@nec.com.au>
2019-02-07 21:33:33 +1030

    fix variable name

--- tasks/refresh_fact.rb
+++ tasks/refresh_fact.rb
@@ -56,7 +56,7 @@ def err(code, kind, message, starttime)
 end
 
 # Update the fact cache
-clean_out, stderr, status = Open3.capture3('/usr/local/bin/os_patching_fact_generation.sh')
+refresh_out, stderr, status = Open3.capture3('/usr/local/bin/os_patching_fact_generation.sh')
 err(status, 'os_patching/fact_cache_update', stderr, starttime) if status != 0
-output(status, 'Patching fact cache updated', clean_out, starttime)
+output(status, 'Patching fact cache updated', refresh_out, starttime)
 log.info 'Patching fact cache updated'
